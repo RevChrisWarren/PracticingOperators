d38dd164b5432f1a96fd53e1b7585ced4e2ce2b3
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-20 13:09:52 -0600

    operator assignments

---
+++ PracticingOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿class Operators
+{
+    static void Main()
+    {
+        double radius = 13;
+        double areaOfCircle = (3.14 * radius * radius);
+        System.Console.WriteLine(areaOfCircle);
+
+        System.Console.ReadKey();
+    }
+}
+
